5b4a4687d0ce2fbce1364d87c6222d15d9e59062
Shannon Trainor <sltrainor@outlook.com>
2020-04-18 10:08:23 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-// array of lower case
-// array of upper case
-// array of special characters
-// array of numbers
+//Array of special characters, numbers, lowercase, uppercase
 
+var specialCharacters = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', ':', ';', '<', ',', '>', ".", '?', '/']
+var numericNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-// Array of special characters to be included in password
-var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+function generatePassword() {
+    //variable to store length of pw
+    var length = prompt("How many characters would you like your password to be?")
+    console.log(length)
 
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    //conditional statement checking is length is number
+    if (isNaN(length) === true) {
+        alert("Password length must be a number")
+        return
+    }
 
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-function generatePasswordOptions() {
-
-  // variable to store length of pw coming from user input response
-  var length = prompt("How many characters would you like your password to be?")
-  console.log(length)
-
-  // conditional statement checking if length is number
-  // if length === true, user did not enter a number
-  // ends with return to start user again
-  if (isNaN(length) === true) {
-    alert("Password length must be a number")
-    return
-  }
 
   // conditional statement to check if length is less than 8
   if (length < 8) {
